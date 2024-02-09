478b3532465f1323d276c4fa21de2773a83f0cd5
Andrew Berlandbor <141494909+berlandbor@users.noreply.github.com>
2024-02-09 12:56:04 +0200

    Update API-listGenerator.js

--- API-listGenerator.js
+++ API-listGenerator.js
@@ -7,8 +7,8 @@ const myListGenerator = new ListGenerator("myListContainer");
 
 const myListItems = [
   /*{ text: "Что такое информатика?", link: "https://berlandbor.github.io/Informatics.md" },*/
-  { text: "Мой блог", link: "https://t.me/BerlandborBlog" },
-  { text: "Учебник по Python", link: "https://t.me/PythonTextbook" },
+  { text: "Обученный Нейрон управляет мячиком:", link: "NeuronBall.html" },
+  { text: "Генератор открытки: ", link: "https://berlandbor.github.io/gen-img/" },
   { text: "Заметки программиста", link: "https://t.me/NotesByProgrammer" }
 ];
 

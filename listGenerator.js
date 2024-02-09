73e01f9f4406a2720f824a3a9a611a6c0867800a
Andrew Berlandbor <141494909+berlandbor@users.noreply.github.com>
2024-02-09 18:19:17 +0200

    Update listGenerator.js

--- listGenerator.js
+++ listGenerator.js
@@ -17,7 +17,7 @@ class ListGenerator {
     this.listContainer.appendChild(ul);
   }
 
-  createListItem(item) {
+  /*createListItem(item) {
     const li = document.createElement("li");
 
     const link = document.createElement("a");
@@ -27,7 +27,22 @@ class ListGenerator {
     li.appendChild(link);
 
     return li;
-  }
+  }*/
+createListItem(item) {
+  const li = document.createElement("li");
+
+  const link = document.createElement("a");
+  link.appendChild(document.createTextNode(item.text));
+  link.href = item.link;
+  link.addEventListener("click", function(event) {
+    event.preventDefault(); // предотвращаем стандартное поведение ссылки
+    window.open(item.link, "_blank"); // открываем ссылку в новой вкладке
+  });
+
+  li.appendChild(link);
+
+  return li;
+}
 }
 
 export { ListGenerator };
